085c32f7a36090fe8e484bcaba736c3023d22f1c
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-09 21:55:34 -0400

    Basic exporting of modules explained.

--- notes-node-app/app.js
+++ notes-node-app/app.js
@@ -1,14 +1,17 @@
-console.log("Starting app");
+console.log("Starting app.js");
 
 // Fetching the File System module
 const fs = require('fs');
 // Fetching the Operating System module
 const os = require('os');
 
-var user = os.userInfo();
-//console.log(user);
+const notes = require('./notes.js');
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
+var user = os.userInfo();
+// console.log(user);
+//console.log(`You are ${notes.age} yrs old.`);
+
+fs.appendFile('greetings.txt', `Hello ${user.username}, you are ${notes.age}.`, function(err) {
   if(err) {
     console.log('Unable to write to file');
   }
