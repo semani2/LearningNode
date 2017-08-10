459062563473144d518a4332c772a91d37294387
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-09 21:34:37 -0400

    Using require to get modules, using the fs and os modules to create a new file and get username from the os module.

--- notes-node-app/app.js
+++ notes-node-app/app.js
@@ -2,8 +2,13 @@ console.log("Starting app");
 
 // Fetching the File System module
 const fs = require('fs');
+// Fetching the Operating System module
+const os = require('os');
 
-fs.appendFile('greetings.txt', 'hello world!', function(err) {
+var user = os.userInfo();
+//console.log(user);
+
+fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
   if(err) {
     console.log('Unable to write to file');
   }
