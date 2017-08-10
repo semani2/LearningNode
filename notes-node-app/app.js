a1b61bd2885eeae86d535f4d0d64d0f079c7f309
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-10 15:44:03 -0400

    Clean up!

--- notes-node-app/app.js
+++ notes-node-app/app.js
@@ -2,19 +2,7 @@ console.log("Starting app.js");
 
 // Fetching the File System module
 const fs = require('fs');
-// Fetching the Operating System module
-const os = require('os');
 const _ = require('lodash');
+
+//Local modules
 const notes = require('./notes.js');
-
-console.log(_.isString(true));
-console.log(_.isString('Sai'));
-
-var filteredArray = _.uniq(['Sai',1,'Sai',1,2,3,4]);
-console.log(filteredArray);
-//var user = os.userInfo();
-// fs.appendFile('greetings.txt', `Hello ${user.username}, you are ${notes.age}.`, function(err) {
-//   if(err) {
-//     console.log('Unable to write to file');
-//   }
-// });
