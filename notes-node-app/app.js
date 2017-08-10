8ee9a3d1584e57a0b92c4e397b6be812d36c2a78
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-10 15:33:28 -0400

    Using sample methods with lodash. (3rd party)

--- notes-node-app/app.js
+++ notes-node-app/app.js
@@ -7,6 +7,11 @@ const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
 
+console.log(_.isString(true));
+console.log(_.isString('Sai'));
+
+var filteredArray = _.uniq(['Sai',1,'Sai',1,2,3,4]);
+console.log(filteredArray);
 //var user = os.userInfo();
 // fs.appendFile('greetings.txt', `Hello ${user.username}, you are ${notes.age}.`, function(err) {
 //   if(err) {
