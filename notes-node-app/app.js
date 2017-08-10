7762e2da14589dda084cbf04c2e85650e8c29da4
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-10 16:10:49 -0400

    Basic implementation of command line args for reading commands.

--- notes-node-app/app.js
+++ notes-node-app/app.js
@@ -6,3 +6,22 @@ const _ = require('lodash');
 
 //Local modules
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log(`Command: ${command}`);
+
+if(command === 'add') {
+  console.log('Adding new note');
+}
+else if(command === 'list') {
+  console.log('Listing all notes');
+}
+else if(command === 'read') {
+  console.log('Here\' your note');
+}
+else if(command === 'remove') {
+  console.log('Removing note');
+}
+else {
+  console.log('Command not recognized');
+}
