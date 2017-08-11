947a0976b96ce366a926de4bd3f4754a89d80bb7
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-11 11:15:06 -0400

    Adding note with check for duplicates implemented.

--- notes-node-app/notes.js
+++ notes-node-app/notes.js
@@ -1,7 +1,26 @@
 console.log("Starting notes.js");
 
+const fs = require('fs');
+
 var addNote = (title, body) => {
-  console.log(`Adding note ${title}, ${body}`);
+  //console.log(`Adding note ${title}, ${body}`);
+  var notes = [];
+  var note = {
+    title, // Shorthand in ES6
+    body
+  };
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch(e) {
+
+  }
+
+  var duplicateNotes = notes.filter((note) => note.title === title); // ES6 shorthand for arrow functions
+  if(duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
