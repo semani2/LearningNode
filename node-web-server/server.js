d90a66225ba9eb35cbabd52a257a627227e49072
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-15 08:51:35 -0400

    Logging and maintenance middle ware implemented.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,13 +1,33 @@
  const express = require('express');
  const hbs = require('hbs');
+ const fs = require('fs');
 // Setting up the express app
  var app = express();
  //Setting up handle bars
  hbs.registerPartials(__dirname + '/views/partials');
  app.set('view engine', 'hbs');
 
-//Setting up a static web page
- app.use(express.static(__dirname + '/public'));
+// Using a middle ware to intersect requests
+//Logger middleware
+ app.use((req, res, next) => {
+   var now = new Date().toString();
+   var log = `${now}: ${req.ip} - ${req.method} ${req.url}`;
+   console.log(log);
+   fs.appendFile('server.log', log + '\n', (err) => {
+     if(err) {
+       console.log('Unable to append to server.log file');
+     }
+   })
+   next();
+ });
+
+// maintenance middle ware
+ app.use((req, res, next) => {
+   res.render('maintenance.hbs');
+ });
+
+ //Setting up a static web page - using middleware
+  app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
