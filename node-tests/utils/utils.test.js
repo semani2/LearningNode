b1f147ca00e1ba31ba2e2208f5d79a57d2a02d8d
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-15 10:55:03 -0400

    Trying out other assertions.

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -12,3 +12,15 @@ it('should square the number', () => {
   var result = utils.square(-5);
   expect(result).toBe(25).toBeA('number');
 });
+
+it('should expect some value', () => {
+  //expect(12).toNotBe(11);
+  //expect({name: 'Sai'}).toEqual({name: 'Sai'});
+  //expect([2,3,4]).toInclude(3);
+  expect({
+    name: "sai",
+    age: 25,
+    location: "Raleigh"}).toInclude({
+      age: 25
+  });
+});
