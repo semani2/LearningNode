938c5bf123681e63df7e3fbaadab2752a9733aa3
Sai Prajnan Emani <sai.emani@citrix.com>
2017-08-15 10:43:11 -0400

    Chaining assertions.

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -5,10 +5,10 @@ const expect = require('expect');
 it('should add two numbers', () => {
   var result = utils.add(11,33);
 
-  expect(result).toBe(44);
+  expect(result).toBe(44).toBeA('number');
 });
 
 it('should square the number', () => {
   var result = utils.square(-5);
-  expect(result).toBe(25);
+  expect(result).toBe(25).toBeA('number');
 });
